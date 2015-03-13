15b2079e2c237793a64810f80d4c2bcdb681ac82
Katie <katiemcgoo@hotmail.co.uk>
2015-03-13 12:51:13 +0000

    updated registration

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
 <?php    
 
     if(isset($_POST["submit"])) {
-        $username = ($_POST["user_name"]);
-        $password = ($_POST["user_password"]);
-        $city = ($_POST["user_city"]);
+        $username = ($_POST["username"]);
+        $password = ($_POST["password"]);
+        $city = ($_POST["city"]);
 
         $query = "INSERT INTO users (username, password, city) VALUES ('{$username}','{$password}','{$city}')";
 
@@ -38,23 +38,7 @@
             <p>Don't have an account? <a href=register.php>Register</a> </p>
         </div>
 
- <div class="register-toggle">
-     <p>Register</p>
-</div>
-     <div class="register">
-            <form action="index.php" method="post">
-                <p>Forename:</p><input type="text" name="user_forename" value=""/><br>
-                <p>Surname:</p><input type="text" name="user_surname" value=""/><br>
-                <p>Date Of Birth:</p><input type="text" name="user_dob" value=""/><br>
-                <p>City:</p><input type="text" name="user_city" value=""/><br>
-                <p>Username:</p><input type="text" name="user_name" value=""/><br>
-                <p>Password:</p><input type="text" name="user_password" value=""/><br>
-                <br>
-                <input type="submit" name="submit" value="Register"/>
-            </form>
-        </div>
-
-
+ 
 
 
 <?php
