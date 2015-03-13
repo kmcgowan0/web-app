577b71984d4be4df0010fea28983840d1a9bea8c
kmcgowan0 <katiemcgoo@hotmail.co.uk>
2015-03-13 11:23:05 +0000

    Bios page

---
+++ about.php
@@ -0,0 +1,30 @@
+<?php require_once("../includes/session.php"); ?>
+<?php require_once("../includes/connect.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+<?php include_once("../includes/templates/header.php"); ?>
+         
+<link rel="stylesheet" href="css/about.css">
+        <div id="container">
+             <ul class="img-list">
+                
+                 <li>
+                   <img src="images/billy.jpg" height="300px" width="300px" alt="Billy Farroll"/>
+                    <span class="text-content"><span>Billy Farroll<br>Bournemouth University student<br> From Southampton<br> Age: 20 <br> Goals: Game Development </span></span>
+                 </li>
+                 <li>
+                    <img src="images/kt2.png" height="300px" width="300px" alt="Katie"/>
+                    <span class="text-content"><span>Katie Mcgowan<br>Bournemouth University student<br> From Northern Ireland<br> Age:18 <br> Goals: Who even knows at this stage tbh</span></span>
+                 </li>
+                 <li>
+                    <img src="images/bankole.jpg" height="300px" width="300px" alt="Banks"/>
+                    <span class="text-content"><span>Banks<br>Bournemouth University student<br> From Southampton</span></span>
+                 </li>
+                 
+             </ul>
+             
+         </div>
+     </body>
+
+<footer class="footer"> <p>Thank you for reading our bios</p></footer>
+
+</html>  
