96b3ae00424234784649f88db0cabab4ef1d7e30
Billy Farroll <watanight@hotmail.co.uk>
2015-04-08 14:04:02 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
-<?php require_once("../includes/session.php"); ?>
-<?php require_once("../includes/connect.php"); ?>
-<?php require_once("../includes/functions.php"); ?>
-<?php include_once("../includes/templates/header.php"); ?>
+<?php require_once("session.php"); ?>
+<?php require_once("connect.php"); ?>
+<?php require_once("functions.php"); ?>
+<?php include_once("header.php"); ?>
 
 
 
@@ -11,87 +11,80 @@
         $surname = ucfirst($_POST["user_surname"]);
         $dob = ($_POST["user_dob"]);
         $city = ucfirst($_POST["user_city"]);
-        $username = ($_POST["user_name"]);
+        $username = ucfirst($_POST["user_name"]);
         $password = ($_POST["user_password"]);
         
-        
-        
-            $query = "INSERT INTO users (user_forename, user_surname, user_dob, user_city, user_name, user_password) VALUES ('{$forename}', '{$surname}', '{$dob}', '{$city}', '{$username}', '{$password}')";
+        $query = "INSERT INTO users (user_forename, user_surname, user_dob, user_city, user_name, user_password) VALUES ('{$forename}','{$surname}','{$dob}','{$city}','{$username}','{$password}',)";
         $result = mysqli_query($connection, $query);
         
         if($result) {
-            $message = "Account successfully created";
+            $message = "Post added";
         } else {
             $message = "Something went wrong";
         }
-            
-        }
-    
-ini_set('session.bug_compat_warn', 0);
-ini_set('session.bug_compat_42', 0);
+    } 
 ?>
+   
+<link rel="stylesheet" href="normal.css" type="text/css"> 
 
-<?php if(!isset($_SESSION["user_name"])) { ?> 
-    <div class="login-toggle">
-        <p>Login</p>
-    </div>
+<div class="login-toggle">
+    <p11 class="forename">Login</p11>
+</div>
+        <div class="login-box">
+            <form action="login.php" method="post">
+                <p1 class="forename">Username:</p1> <input type="text" name="username" value="" /> <br>
+                <p2 class="forename">Password:</p2> <input type="password" name="password" value="" /> <br>
+                <br>
+                <input type="submit" name="login" value="Login" />
+            </form>
+            <p3 class="forename">Don't have an account? <a href=register.php>Register</a> </p3>
+        </div>
 
-    <div class="login-box">
-        <form action="login.php" method="post">
-            <p>Username:</p> <input type="text" name="user_name" value="" /> <br>
-            <p>Password:</p> <input type="password" name="user_password" value="" /> <br>
-            <br>
-            <input type="submit" name="login" value="Login" />
-        </form>
-            
-    </div>
+ <div class="register-toggle">
+     <p4 class="forename">OR <br> Register</p4>
+</div>
+     <div class="register">
+            <form action="index.php" method="post">
+                <p5 class="forename">Forename:</p5><input type="text" name="user_forename" value=""/><br>
+                <p6 class="forename">Surname:</p6><input type="text" name="user_surname" value=""/><br>
+                <p7 class="forename">Date Of Birth:</p7><input type="text" name="user_dob" value=""/><br>
+                <p8 class="forename">City:</p8><input type="text" name="user_city" value=""/><br>
+                <p9 class="forename">Username:</p9><input type="text" name="user_name" value=""/><br>
+                <p10 class="forename">Password:</p10><input type="text" name="user_password" value=""/><br>
+                <br>
+                <input type="submit" name="submit" value="Register"/>
+                <div class=""
+            </form>
+            <div class="overlay black-bg">  
+        </div>
 
-    <div class="register-toggle">
-        <p>Don't have an account? <br> Register</p>
-    </div>
 
-    <div class="register">
-        <form action="index.php" method="post">
-            <p>Forename:</p><input type="text" name="user_forename" value=""/><br>
-            <p>Surname:</p><input type="text" name="user_surname" value=""/><br>
-            <p>Date Of Birth:</p><input type="date" name="user_dob" value=""/><br>
-            <p>City:</p><input type="text" name="user_city" value=""/><br>
-            <p>Username:</p><input type="text" name="user_name" value=""/><br>
-            <p>Password:</p><input type="text" name="user_password" value=""/><br>
-            <br>
-            <input type="submit" name="submit" value="Register"/>
-        </form>
-    </div>
 
-<?php } ?>
-
-<?php if(isset($_SESSION["user_name"])) { ?>
-    
-    <div class="holidays">
-        <p><a href="holidays.php">Now browse our holidays, or add one of your own</a></p>
-    </div>
-
-    <div class="logout-toggle">
-        <p>Logout</p>
-     </div>
-
-    <div class="logout">
-        <p><a href="logout.php">Are you sure? Logout</a></p>
-    </div>
-
-<?php } ?>
+      
+      
 
 <?php
-    if(isset($_POST["submit"])) { ?>
+    if(isset($_POST["submit"])) {
+        echo "Forename: $forename " ;
+        echo "Surname: $surname " ;
+        echo "Date of birth: $dob " ;
+        echo "City: $city " ;
+        echo "Username: $username " ; 
+        echo "Password: $password " ; 
         
-        <div class="holidays">
-            <p><?php echo $message; ?>
-            <a href="holidays.php">Now browse our holidays, or add one of your own</a></p>
-        </div>
+    }
+?>
+
+<?php
+    while($row = my_fetch_assoc($result)) {
         
- <?php  } ?>
+     include 'box.php';
+        
+    }
+?>
 
 
 
+
         
-<?php require_once("../includes/templates/footer.php");?>      
+<?php require_once("footer.php"); ?>
