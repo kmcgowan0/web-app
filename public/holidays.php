a5f8214e9e5d44af637c5632317608d5de5f0b84
Billy Farroll <watanight@hotmail.co.uk>
2015-04-08 14:06:07 +0100

    Update holidays.php

--- public/holidays.php
+++ public/holidays.php
@@ -1,15 +1,17 @@
-<?php require_once("../includes/session.php"); ?>
-<?php require_once("../includes/connect.php"); ?>
-<?php require_once("../includes/functions.php"); ?>
-<?php include_once("../includes/templates/header.php"); ?>
+<?php require_once("session.php"); ?>
+<?php require_once("connect.php"); ?>
+<?php require_once("functions.php"); ?>
+<?php include_once("header.php"); ?>
 <?php    
     if(isset($_POST["submit"])) {
         $location = ucfirst($_POST["holiday_location"]);
         $description = ucfirst($_POST["holiday_description"]);
         $rating = ($_POST["holiday_rating"]);
+        $tags = ($_POST["holiday_tags"]);
+        $picture = ($_POST["holiday_picture"]);
         $holuser = ucfirst($_POST["holiday_user"]);
         
-        $query = "INSERT INTO holiday (holiday_location, holiday_description, holiday_rating, holiday_user) VALUES ('{$location}','{$description}','{$rating}','{$holuser}')";
+        $query = "INSERT INTO holiday (holiday_location, holiday_description, holiday_rating, holiday_tags, holiday_picture, holiday_user) VALUES ('{$location}','{$description}','{$rating}','{$tags}','{$picture}','{$holuser}')";
         $result = mysqli_query($connection, $query);
         
         if($result) {
@@ -18,12 +20,10 @@
             $message = "Something went wrong";
         }
     } 
-ini_set('session.bug_compat_warn', 0);
-ini_set('session.bug_compat_42', 0);
 ?>
 
 
-
+<link rel="stylesheet" href="normal.css" type="text/css">
 
         
 <div class="container">            
@@ -31,62 +31,76 @@ ini_set('session.bug_compat_42', 0);
             <?php $title = "Box"; ?>
             
             
+            
+            <?php if(isset($message)) { ?>
+                
+                <div class="box">
+                    <p><?php echo $message; ?></p>
+                </div>
+            
+            <?php } ?>
 
 <div class="holiday-toggle">
-    <p>Add A Holiday</p>
+    <p class="forename">Add A Holiday</p>
 </div>
 <div class="add-holiday">
-    <form action="holidays.php" method="post">
-        <p>Location:</p><input type="text" name="holiday_location" value=""/><br>
-        <p>Description:</p><input type="text" name="holiday_description" value=""/><br>
-        <p>Rating /5:</p> <input type="number" name="holiday_rating" min="0" max="5" value=""/>
-      
-        <p>Name:</p><input type="text" name="holiday_user" value=""/><br>
-        <br>
-        <input type="submit" name="submit" value="Add"/>
-    </form>
-</div>
-
-
-
-
-        <div class="sort">
-            <p>Sort Holidays</p>
             <form action="holidays.php" method="post">
-                <select name="sort-by">
-                    <option value="new">Most Recent</option>
-                    <option value="best">By Rating</option>
+                <p class="forename">Location:</p><input type="text" name="holiday_location" value=""/><br>
+                <p class="forename">Description:</p><input type="text" name="holiday_description" value=""/><br>
+                <p class="forename">Rating:</p> <select name="holiday_rating">
+                <option value="">--Select--</option>
+                <option value="">0</option>
+                <option value="">1</option>
+                <option value="">2</option>
+                <option value="">3</option>
+                <option value="">4</option>
+                <option value="">5</option>
                 </select>
-                <input type="submit" name="sort" value="Sort" />
+                <p class="forename">Tags:</p><input type="text" name="holiday_tags" value=""/><br>
+                <p class="forename">Username:</p><input type="text" name="holiday_user" value=""/><br>
+                <br>
+                
+                <p class="forename">Add an image?</p><input type="file" name="holiday_picture" id="holiday_picture"><br>
+              
+                <input type="submit" name="submit" value="Add"/>
             </form>
         </div>
     
+<style>
+body {
+    background-image: url("holiday.jpg");
+}
+</style>
+
+<style>
+
+h1 {
+    text-align: center;
+}
+
+p.date {
+    text-align: right;
+}
+
+p.main {
+    text-align: justify;
+}
+       
+       </style>
 
 
 
-<!--?php
+<?php
     if(isset($_POST["submit"])) {
         echo "Location: $location " ;
         echo "Description: $description " ;
         echo "Rating: $rating /5 " ;
         echo "Tags: $tags " ;
-        
+        echo "Picture: $picture " ; 
         echo "Username: $holuser " ; 
         
     }
-?>-->
-<?php if(isset($message)) { ?>
-
-    <div class="success">
-        <p><?php echo $message; ?></p>
-    </div>
-
-<?php } ?>
-    
-    <?php 
-            include "box.php"    
-                
-            ?>
+?>
 
         
-<?php include_once("../includes/templates/footer.php"); ?>  
+<?php include_once("footer.php"); ?>  
